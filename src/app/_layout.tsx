2540e26c9c409dec099e74a07c4f08ff0cdd8b59
Adam Seifert-Page <adamseifertpage@gmail.com>
2026-06-09 23:16:08 +0100

    build: add react-query and provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,30 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { DarkTheme, DefaultTheme, Tabs, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
 
 import { Colors } from '@/constants/theme';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const scheme = useColorScheme();
   const colors = Colors[scheme === 'dark' ? 'dark' : 'light'];
 
   return (
-    <ThemeProvider value={scheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: colors.text,
-          tabBarInactiveTintColor: colors.textSecondary,
-          tabBarStyle: { backgroundColor: colors.background },
-        }}>
-        <Tabs.Screen name="upload" options={{ title: 'Upload' }} />
-        <Tabs.Screen name="index" options={{ title: 'Home' }} />
-        <Tabs.Screen name="favourites" options={{ title: 'Favourites' }} />
-      </Tabs>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: colors.text,
+            tabBarInactiveTintColor: colors.textSecondary,
+            tabBarStyle: { backgroundColor: colors.background },
+          }}>
+          <Tabs.Screen name="upload" options={{ title: 'Upload' }} />
+          <Tabs.Screen name="index" options={{ title: 'Home' }} />
+          <Tabs.Screen name="favourites" options={{ title: 'Favourites' }} />
+        </Tabs>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
